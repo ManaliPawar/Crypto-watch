5b0f6ac6fe15ab4b35b5364633fece858c90cdfb
Ayush-AT24 <ayush.tiwari.met20@itbhu.ac.in>
2022-07-01 17:25:18 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+        apiKey: "AIzaSyBwU_ziKCpZOYhgeyQ_xrBGPaIhoo5trIA",
+        authDomain: "crypto-watch-f4476.firebaseapp.com",
+        projectId: "crypto-watch-f4476",
+        storageBucket: "crypto-watch-f4476.appspot.com",
+        messagingSenderId: "58747318606",
+        appId: "1:58747318606:web:f356600db6f5204322ae1f"
+      };
+      
+export default firebaseConfig;
